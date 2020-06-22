34fab3e84d059a7eb45f0f56b790bad75f61070e
Cyril <cyril.marcus@emirates.com>
2020-06-22 23:52:49 +0000

    fix template error

--- src/pages/template-page.js
+++ src/pages/template-page.js
@@ -8,22 +8,23 @@ import Layout from "../components/layout"
 
 const Template = ({ data: { template } }) => (
   <Layout>
-    <article className="sheet">
+    test
+    {/* <article className="sheet">
     <Img style={{borderRadius: "20%"}} fluid={template.photo.fluid} />
-    </article>
+    </article> */}
   </Layout>
 )
 
 export default Template
 
-export const query = graphql`
-  query TemplateQuery {
-    template: datoCmsTemplatePage {
-      photo {
-        fluid(maxWidth: 1000,  imgixParams: {maxW: 1000, fm: "png", auto: "compress" }){
-          ...GatsbyDatoCmsFluid
-        }
-      }
-    }
-  }
-`
+// export const query = graphql`
+//   query TemplateQuery {
+//     template: datoCmsTemplatePage {
+//       photo {
+//         fluid(maxWidth: 1000,  imgixParams: {maxW: 1000, fm: "png", auto: "compress" }){
+//           ...GatsbyDatoCmsFluid
+//         }
+//       }
+//     }
+//   }
+// `
