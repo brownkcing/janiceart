75c33d3b7eacbbf70cd843ada394642a7babd716
Cyril <cyril.marcus@emirates.com>
2020-06-23 01:21:43 +0000

    rewrk 0n tempate page

--- src/pages/template-page.js
+++ src/pages/template-page.js
@@ -6,20 +6,47 @@ import Layout from "../components/layout"
 
 
 
-const Template = ({ data: { template } }) => (
+const About = ({ data: { about } }) => (
   <Layout>
-     <article className="sheet">
-     <p className="sheet__lead">{template.test}</p>
+    <article className="sheet">
+      <HelmetDatoCms seo={about.seoMetaTags} />
+      <div className="sheet__inner">
+        {/* <h1 className="sheet__title">{about.title}</h1> */}
+        <p className="sheet__lead">{about.subtitle}</p>
+        <div className="sheet__gallery">
+         <Img style={{borderRadius: "20%"}} fluid={about.photo.fluid} />
+        </div>
+        <div
+          className="sheet__body"
+          dangerouslySetInnerHTML={{
+            __html: about.bioNode.childMarkdownRemark.html,
+          }}
+        />
+      </div>
     </article>
   </Layout>
 )
 
-export default Template
+export default About
 
 export const query = graphql`
-  query TemplateQuery {
-    template: datoCmsTemplatePage {
-      test
+  query AboutQuery {
+    about: datoCmsAboutPage {
+      seoMetaTags {
+        ...GatsbyDatoCmsSeoMetaTags
+      }
+      title
+      subtitle
+      photo {
+        fluid(maxWidth: 1000,  imgixParams: {maxW: 1000, fm: "png", auto: "compress" }){
+          ...GatsbyDatoCmsFluid
+        }
+      }
+      bioNode {
+        childMarkdownRemark {
+          html
+        }
+      }
     }
   }
 `
