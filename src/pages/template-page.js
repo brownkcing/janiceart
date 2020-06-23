d19e6a86832f681e1f681fd38c5ed70fa3a2803f
Cyril <cyril.marcus@emirates.com>
2020-06-23 00:15:26 +0000

    rewrk 0n tempate page

--- src/pages/template-page.js
+++ src/pages/template-page.js
@@ -1,47 +1,9 @@
-import React from 'react';
-import { Link, graphql } from 'gatsby';
-import Masonry from 'react-masonry-component';
-import Img from 'gatsby-image';
-import Layout from "../components/layout";
+import React from 'react'
 
-
-const Template= ({ data }) => (
-
-  <Layout>
-    <Masonry className="showcase">
-      {data.allDatoCmsWork.edges.map(({ node: work }) => ( 
-        <div key={work.id} className="showcase__item">
-          <figure className="card">
-            <div className="card__image">
-            <Link state={{modal:true, noScroll:true}} to={`/works/${work.slug}`} className="card__image">
-              <Img fluid={work.coverImage.fluid} />
-            </Link>
-            </div>
-            </figure>
-        </div>
-      ))}
-    </Masonry>
-  </Layout>
-  
+const testing= () => (
+  <div>
+   Hell000000000000000000000000000
+  </div>
 )
 
-export default Template
-
-export const query = graphql`
-  query IndexQuery {
-    allDatoCmsWork(sort: { fields: [position], order: DESC }) {
-      edges {
-        node {
-          id
-          title
-          slug
-          coverImage {
-            fluid(maxWidth: 400, maxHeight: 400, imgixParams: { fm: "jpg", auto: "compress" }) {
-              ...GatsbyDatoCmsSizes
-            }
-          }
-        }
-      }
-    }
-  }
-`
+export default testing
