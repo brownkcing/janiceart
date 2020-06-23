1dafaf5c12b1dcfa210516312b41e6e923072004
Cyril <cyril.marcus@emirates.com>
2020-06-23 00:27:10 +0000

    rewrk 0n tempate page

--- src/pages/template-page.js
+++ src/pages/template-page.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import { graphql } from 'gatsby'
+import { HelmetDatoCms } from 'gatsby-source-datocms'
+import Img from 'gatsby-image'
+import Layout from "../components/layout"
 
-const testing= () => (
-  <div>
-   Hell000000000000000000000000000
-  </div>
+
+
+const Template = ({ data: { template } }) => (
+  <Layout>
+     <article className="sheet">
+     <p className="sheet__lead">{template.test}</p>
+    </article>
+  </Layout>
 )
 
-export default testing
+export default Template
+
+export const query = graphql`
+  query TemplateQuery {
+    template: datoCmsTemplatePage {
+      test
+    }
+  }
+`
